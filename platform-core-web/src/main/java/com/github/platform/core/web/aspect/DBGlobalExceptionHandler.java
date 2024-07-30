717ed3e8012eafb955ec8caa0d0b4b041d7fd71f
yxkong <5ycode@sina.com>
2024-07-30 16:07:50 +0800

    异常处理优化

--- platform-core-web/src/main/java/com/github/platform/core/web/aspect/DBGlobalExceptionHandler.java
+++ platform-core-web/src/main/java/com/github/platform/core/web/aspect/DBGlobalExceptionHandler.java
@@ -1,7 +1,80 @@
-package com.github.platform.core.web.aspect;/**
- * <TODO>
+package com.github.platform.core.web.aspect;
+
+import com.github.platform.core.common.aspect.ExceptionHandlerBase;
+import com.github.platform.core.common.configuration.property.PlatformProperties;
+import com.github.platform.core.common.constant.SpringBeanOrderConstant;
+import com.github.platform.core.standard.constant.HeaderConstant;
+import com.github.platform.core.standard.constant.ResultStatusEnum;
+import com.github.platform.core.standard.entity.dto.ResultBean;
+import com.github.platform.core.standard.exception.InfrastructureException;
+import com.github.platform.core.standard.util.ExceptionUtil;
+import com.github.platform.core.standard.util.ResultBeanUtil;
+import com.github.platform.core.web.util.RequestHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.CannotAcquireLockException;
+import org.springframework.dao.DeadlockLoserDataAccessException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.QueryTimeoutException;
+import org.springframework.jdbc.BadSqlGrammarException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 数据库异常处理
  * @author: yxkong
  * @date: 2024/7/8 18:43
  * @version: 1.0
- */public class DBGlobalExceptionHandler {
+ */
+@RestControllerAdvice
+@Order(SpringBeanOrderConstant.GLOBAL_EXCEPTION)
+@Slf4j
+@ConditionalOnClass(name = {"org.springframework.dao.QueryTimeoutException;", "org.springframework.jdbc.BadSqlGrammarException"})
+public class DBGlobalExceptionHandler  extends ExceptionHandlerBase {
+    @Resource
+    private PlatformProperties properties;
+    /******* 数据库异常处理  *******/
+    @ConditionalOnClass(BadSqlGrammarException.class)
+    @ExceptionHandler(BadSqlGrammarException.class)
+    public void badSqlGrammarException(BadSqlGrammarException e) {
+        throw new InfrastructureException(ResultStatusEnum.DB_BAD_SQL,e);
+    }
+    @ConditionalOnClass(CannotGetJdbcConnectionException.class)
+    @ExceptionHandler(CannotGetJdbcConnectionException.class)
+    public void cannotGetJdbcConnectionException(CannotGetJdbcConnectionException e) {
+        throw new InfrastructureException(ResultStatusEnum.DB_BAD_SQL,e);
+    }
+    @ConditionalOnClass(DuplicateKeyException.class)
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResultBean<?> duplicateKeyException(DuplicateKeyException e) {
+        log("duplicateKeyException", e);
+        return ResultBeanUtil.result(ResultStatusEnum.DB_DUPLICATE_KEY, ExceptionUtil.getMessage(e));
+    }
+    @ConditionalOnClass(DeadlockLoserDataAccessException.class)
+    @ExceptionHandler(DeadlockLoserDataAccessException.class)
+    public ResultBean<?> deadlockLoserDataAccessException(DeadlockLoserDataAccessException e) {
+        log("deadlockLoserDataAccessException", e);
+        return ResultBeanUtil.result(ResultStatusEnum.DB_DEAD_LOCK, ExceptionUtil.getMessage(e));
+    }
+    @ConditionalOnClass(QueryTimeoutException.class)
+    @ExceptionHandler(QueryTimeoutException.class)
+    public ResultBean<?> queryTimeoutException(QueryTimeoutException e) {
+        log("queryTimeoutException", e);
+        return ResultBeanUtil.result(ResultStatusEnum.DB_QUERY_TIMEOUT, ExceptionUtil.getMessage(e));
+    }
+    @ConditionalOnClass(CannotAcquireLockException.class)
+    @ExceptionHandler(CannotAcquireLockException.class)
+    public ResultBean<?> cannotAcquireLockException(CannotAcquireLockException e) {
+        log("cannotAcquireLockException", e);
+        return ResultBeanUtil.result(ResultStatusEnum.DB_CANNOT_ACQUIRE_LOCK, ExceptionUtil.getMessage(e));
+    }
+    public void log(String exceptionName,Throwable e){
+        HttpServletRequest request = RequestHolder.getRequest();
+        log.error(log(exceptionName,request.getHeader(HeaderConstant.TOKEN), request.getRequestURI(),null,RequestHolder.getIncludeHeaders(request,properties.getIncludeHeaders()),RequestHolder.getRequestBody(request),null),e);
+    }
 }
--- platform-core-web/src/main/java/com/github/platform/core/web/aspect/GlobalExceptionHandler.java
+++ platform-core-web/src/main/java/com/github/platform/core/web/aspect/GlobalExceptionHandler.java
@@ -2,25 +2,18 @@ package com.github.platform.core.web.aspect;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.github.platform.core.common.aspect.ExceptionHandlerBase;
-import com.github.platform.core.common.constant.SpringBeanOrderConstant;
 import com.github.platform.core.common.configuration.property.PlatformProperties;
+import com.github.platform.core.common.constant.SpringBeanOrderConstant;
 import com.github.platform.core.standard.constant.HeaderConstant;
 import com.github.platform.core.standard.constant.ResultStatusEnum;
 import com.github.platform.core.standard.entity.dto.ResultBean;
 import com.github.platform.core.standard.exception.*;
-import com.github.platform.core.standard.util.ExceptionUtil;
 import com.github.platform.core.standard.util.ResultBeanUtil;
 import com.github.platform.core.web.util.RequestHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
-import org.springframework.dao.CannotAcquireLockException;
-import org.springframework.dao.DeadlockLoserDataAccessException;
-import org.springframework.dao.DuplicateKeyException;
-import org.springframework.dao.QueryTimeoutException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
-import org.springframework.jdbc.BadSqlGrammarException;
-import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -31,7 +24,6 @@ import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Arrays;
 import java.util.Objects;
 
@@ -201,35 +193,7 @@ public class GlobalExceptionHandler extends ExceptionHandlerBase{
         log("RuntimeException",e);
         return ResultBeanUtil.fail();
     }
-    /******* 数据库异常处理  *******/
-    @ExceptionHandler(BadSqlGrammarException.class)
-    public void badSqlGrammarException(BadSqlGrammarException e) {
-        throw new InfrastructureException(ResultStatusEnum.DB_BAD_SQL,e);
-    }
-    @ExceptionHandler(CannotGetJdbcConnectionException.class)
-    public void cannotGetJdbcConnectionException(CannotGetJdbcConnectionException e) {
-        throw new InfrastructureException(ResultStatusEnum.DB_BAD_SQL,e);
-    }
-    @ExceptionHandler(DuplicateKeyException.class)
-    public ResultBean<?> duplicateKeyException(DuplicateKeyException e) {
-        log("duplicateKeyException", e);
-        return ResultBeanUtil.result(ResultStatusEnum.DB_DUPLICATE_KEY, ExceptionUtil.getMessage(e));
-    }
-    @ExceptionHandler(DeadlockLoserDataAccessException.class)
-    public ResultBean<?> deadlockLoserDataAccessException(DeadlockLoserDataAccessException e) {
-        log("deadlockLoserDataAccessException", e);
-        return ResultBeanUtil.result(ResultStatusEnum.DB_DEAD_LOCK, ExceptionUtil.getMessage(e));
-    }
-    @ExceptionHandler(QueryTimeoutException.class)
-    public ResultBean<?> queryTimeoutException(QueryTimeoutException e) {
-        log("queryTimeoutException", e);
-        return ResultBeanUtil.result(ResultStatusEnum.DB_QUERY_TIMEOUT, ExceptionUtil.getMessage(e));
-    }
-    @ExceptionHandler(CannotAcquireLockException.class)
-    public ResultBean<?> cannotAcquireLockException(CannotAcquireLockException e) {
-        log("cannotAcquireLockException", e);
-        return ResultBeanUtil.result(ResultStatusEnum.DB_CANNOT_ACQUIRE_LOCK, ExceptionUtil.getMessage(e));
-    }
+
 
 
     private void log(String exceptionName,String exceptionStr,Throwable e){
